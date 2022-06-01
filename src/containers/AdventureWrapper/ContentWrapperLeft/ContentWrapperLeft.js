ac595b0f086205ed40c0185fee3b2800b8d7b03d
Koen H <hoogendoornkoen@gmail.com>
2022-06-01 11:19:41 +0200

    downloadfilesbutton -> downloadfilebutton

--- src/containers/AdventureWrapper/ContentWrapperLeft/ContentWrapperLeft.js
+++ src/containers/AdventureWrapper/ContentWrapperLeft/ContentWrapperLeft.js
@@ -12,7 +12,7 @@ import CardItemEditor from "../../../components/CardItem/CardItemEditor/CardItem
 import TabsContainer from "./TabsContainer/TabsContainer";
 import Modal from "../../../components/Modal/Modal";
 import Logo from "../../../assets/logo/DragonsQuillLogo";
-import DownloadFilesButton from "../../../components/DownloadFilesButton/DownloadFilesButton";
+import DownloadFileButton from "../../../components/DownloadFileButton/DownloadFileButton";
 
 import DeleteItemIllustration from "../../../assets/illustrations/DeleteItemIllustration";
 
@@ -388,7 +388,7 @@ const ContentWrapperLeft = (props) => {
       >
         <div className={classes.HeaderBar}>
           <Logo classes={classes.Logo} />
-          <DownloadFilesButton />
+          <DownloadFileButton />
         </div>
 
         <h1>{props.adventure.value}</h1>
