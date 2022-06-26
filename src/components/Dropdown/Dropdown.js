145d1c7b2c622f653bb429dbc9c76ea575bc6a04
Koen H <hoogendoornkoen@gmail.com>
2022-06-26 17:59:18 +0200

    remove dropdown panel on click outside it

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,20 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classes from "./Dropdown.module.scss";
 
 const Dropdown = (props) => {
   const [showDropdown, setShowDropdown] = useState(false);
 
-  let dropdownContentClasses = showDropdown
-    ? `${classes.DropdownContent} ${classes.Show}`
-    : `${classes.DropdownContent}`;
+  // hide panel if clicked outside of it.
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowDropdown(false);
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mouseup", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mouseup", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  let dropdownContent = showDropdown ? (
+    <div className={classes.DropdownContent} ref={wrapperRef}>
+      {props.children}
+    </div>
+  ) : null;
 
   return (
-    <div
-      className={classes.Dropdown}
-      onClick={() => setShowDropdown(!showDropdown)}
-    >
-      <i className={props.clickableIconName}></i>
-      <div className={dropdownContentClasses}>{props.children}</div>
+    <div className={classes.Dropdown}>
+      <i
+        className={props.clickableIconName}
+        onClick={() => setShowDropdown(!showDropdown)}
+      ></i>
+      {dropdownContent}
     </div>
   );
 };
